aa5cc9febb03e4304764c45f4fbf56f94cde750d
Patryk <patryk1m1@gmail.com>
2019-01-14 19:31:51 +0100

    Add devise,milia and bootstrap, start tenant

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+
+  resources :members
+  get 'home/index'
+  root :to => "home#index"
+
+  # *MUST* come *BEFORE* devise's definitions (below)
+  as :user do
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
+  end
+
+  devise_for :users, :controllers => {
+    :registrations => "milia/registrations",
+    :confirmations => "confirmations",
+    :sessions => "milia/sessions",
+    :passwords => "milia/passwords",
+  }
+
+
+  resources :user do
+    member do
+      get :confirm_email
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
